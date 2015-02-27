ad891cdcd01a3e0dd539f24b2839b8b8dc622f2a
Nahal <Nahal@LittleTree-pc>
2015-02-27 05:18:21 -0800

    Repository Name_Project Name_Version

--- src/git_sample_2/Git_Sample_2.java
+++ src/git_sample_2/Git_Sample_2.java
@@ -15,7 +15,7 @@ public class Git_Sample_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.err.println("re");
+        
     }
     
 }
